30d9d222a23f6525e1567b6bda0ad6fcbf003b06
m0n5terG <mikotai@yahoo.com>
2021-05-21 19:14:33 +0800

    task7

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export default function App() {
   };
 
   useEffect(() => {
-    const interval = setInterval(loadBusStopData, 10000);
+    const interval = setInterval(loadBusStopData, 15000);
     return () => clearInterval(interval);
   }, []);
 
